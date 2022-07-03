d13cc376a9c7e9443c607f4b5c5af13727b01718
harry-sketch <harshbisht90@gmail.com>
2022-07-03 13:27:10 +0530

    init: repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// Styles
+import "./App.css";
+
+// Components
+import Main from "./components/Main/Main";
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-gradient-to-b from-[#7453BB] to-[#3DAAD5] flex items-center justify-center">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="rounded-lg w-1/3 bg-[#2C2B69] h-4/5">
+      <div className="text-white text-2xl font-bold border-b-2 bg-[#262157] rounded-tl-lg rounded-tr-lg py-4 text-center border-[#262157]">
+        Registration Form
+      </div>
+    </div>
+  );
+};
+
+export default Main;
